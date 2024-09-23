5f41e2b992e1339b53a49bb589468138a09795ea
HamInKyou <hfire1207@gmail.com>
2024-09-23 19:15:15 +0900

    [함인규] 3주차 :: 캔버스 생성 방법 수정

--- 3주차/함인규/src/main.ts
+++ 3주차/함인규/src/main.ts
@@ -1,7 +1,16 @@
 import './style.css'
 
+function createCanvas(width: number, height: number) {
+    const $app = document.getElementById('app')!;
+    const $canvas = document.createElement('canvas');
+    $canvas.width = width;
+    $canvas.height = height;
+    $app.appendChild($canvas);
+    return $canvas;
+}
+
 //선그리기
-const $canvas1 = document.querySelector<HTMLCanvasElement>('#canvas1')!;
+const $canvas1 = createCanvas(400, 300);
 const ctx1 = $canvas1.getContext('2d')!;
 ctx1.beginPath();
 ctx1.moveTo(100, 50);
