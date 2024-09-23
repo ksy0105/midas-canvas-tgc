7c3cebcbcd10df4934bdf9654d934185920f02a7
HamInKyou <hfire1207@gmail.com>
2024-09-23 19:17:24 +0900

    [함인규] 3주차 :: 사각형 그리기

--- 3주차/함인규/src/main.ts
+++ 3주차/함인규/src/main.ts
@@ -17,3 +17,13 @@ ctx1.moveTo(100, 50);
 ctx1.lineTo(300,50);
 ctx1.stroke();
 
+//사각형 그리기
+const $canvas2 = createCanvas(400, 300);
+const ctx2 = $canvas2.getContext('2d')!;
+ctx2.beginPath();
+ctx2.moveTo(100, 50);
+ctx2.lineTo(300, 50);
+ctx2.lineTo(300, 200);
+ctx2.lineTo(100, 200);
+ctx2.lineTo(100, 50);
+ctx2.stroke();
