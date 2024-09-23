2f4b2f9b8f19c6111a8bb5405f851aa89470f479
HamInKyou <hfire1207@gmail.com>
2024-09-23 19:52:10 +0900

    [함인규] 3주차 :: 선 그리기 완료

--- 3주차/함인규/src/main.ts
+++ 3주차/함인규/src/main.ts
@@ -15,7 +15,7 @@ const ctx1 = $canvas1.getContext('2d')!;
 ctx1.beginPath();
 ctx1.moveTo(100, 50);
 ctx1.lineTo(300,50);
-ctx1.stroke();
+ctx1.stroke(); // 선 긋기
 
 //사각형 그리기
 const $canvas2 = createCanvas(400, 300);
@@ -26,4 +26,135 @@ ctx2.lineTo(300, 50);
 ctx2.lineTo(300, 200);
 ctx2.lineTo(100, 200);
 ctx2.lineTo(100, 50);
-ctx2.stroke();
+ctx2.stroke(); // 선 긋기
+
+//내부에 색 채우기 - 검정색
+const $canvas3 = createCanvas(400, 300);
+const ctx3 = $canvas3.getContext('2d')!;
+ctx3.beginPath();
+ctx3.moveTo(100, 50);
+ctx3.lineTo(300, 50);
+ctx3.lineTo(300, 200);
+ctx3.lineTo(100, 200);
+ctx3.lineTo(100, 50);
+ctx3.stroke(); // 선 긋기
+ctx3.fill(); // 채우기
+
+//내부에 색 채우기 - 다른색
+const $canvas4 = createCanvas(400, 300);
+const ctx4 = $canvas4.getContext('2d')!;
+ctx4.beginPath();
+ctx4.moveTo(100, 50);
+ctx4.lineTo(300, 50);
+ctx4.lineTo(300, 200);
+ctx4.lineTo(100, 200);
+ctx4.lineTo(100, 50);
+ctx4.stroke(); // 선 긋기
+ctx4.fillStyle = "red"; // 채울 색 변경
+ctx4.fill();  // 채우기
+
+//선을 다른 색으로 하고 두께 변경하기
+const $canvas5 = createCanvas(400, 300);
+const ctx5 = $canvas5.getContext('2d')!;
+ctx5.beginPath();
+ctx5.moveTo(100, 50);
+ctx5.lineTo(300, 50);
+ctx5.lineTo(300, 200);
+ctx5.lineTo(100, 200);
+ctx5.lineTo(100, 50);
+ctx5.lineWidth = 20; // 그을 선 두께 변경
+ctx5.strokeStyle = 'blue'; // 그을 선 색 변경
+ctx5.stroke(); // 선 긋기
+ctx5.fillStyle = "red";
+ctx5.fill();
+
+//선을 다른 색으로 하고 두께 변경하기
+const $canvas6 = createCanvas(400, 300);
+const ctx6 = $canvas6.getContext('2d')!;
+ctx6.beginPath();
+ctx6.moveTo(100, 50);
+ctx6.lineTo(300, 50);
+ctx6.lineTo(300, 200);
+ctx6.lineTo(100, 200);
+ctx6.lineTo(100, 50);
+ctx6.lineWidth = 20; // 그을 선 두께 변경
+ctx6.strokeStyle = 'blue'; // 그을 선 색 변경
+ctx6.lineCap = 'square'; // 선 끝부분 처리
+ctx6.stroke(); // 선 긋기
+ctx6.fillStyle = "red";
+ctx6.fill();
+
+//선의 세가지 끝부분 처리 방법
+const $canvas7 = createCanvas(400, 300);
+const ctx7 = $canvas7.getContext('2d')!;
+ctx7.lineWidth = 20;
+ctx7.strokeStyle = "blue"
+
+ctx7.beginPath();
+ctx7.moveTo(100, 50);
+ctx7.lineTo(300, 50);
+ctx7.lineCap = "butt"; // 선 끝을 좌표에 맞춰서 마무리 (기본값)
+ctx7.stroke();
+
+ctx7.beginPath();
+ctx7.moveTo(100, 100);
+ctx7.lineTo(300, 100);
+ctx7.lineCap = "round"; // 선 끝을 둥글게 ( 선 두께를 반지름으로 )
+ctx7.stroke();
+
+ctx7.beginPath();
+ctx7.moveTo(100, 150);
+ctx7.lineTo(300, 150);
+ctx7.lineCap = "square"; // 선 끝을 선 두께만큼 더 길게 처리
+ctx7.stroke();
+
+//선의 꺾인 부분 처리
+const $canvas8 = createCanvas(400, 300);
+const ctx8 = $canvas8.getContext('2d')!;
+ctx8.lineWidth = 20;
+ctx8.strokeStyle = "blue";
+
+ctx8.beginPath();
+ctx8.moveTo(100, 50);
+ctx8.lineTo(300, 50);
+ctx8.lineTo(300, 100);
+ctx8.lineJoin = "miter"; // 각진 모서리 형태
+ctx8.stroke();
+
+ctx8.beginPath();
+ctx8.moveTo(100, 150);
+ctx8.lineTo(300, 150);
+ctx8.lineTo(300, 200);
+ctx8.lineJoin = "round"; // 둥글게
+ctx8.stroke();
+
+ctx8.beginPath();
+ctx8.moveTo(100, 250);
+ctx8.lineTo(300, 250);
+ctx8.lineTo(300, 300);
+ctx8.lineJoin = "bevel"; // 잘려나간 모서리
+ctx8.stroke();
+
+
+//점선 만들기
+const $canvas9 = createCanvas(400, 300);
+const ctx9 = $canvas9.getContext('2d')!;
+ctx9.lineWidth = 20;
+
+ctx9.beginPath();
+ctx9.moveTo(100, 50);
+ctx9.lineTo(300, 50);
+ctx9.setLineDash([20]) //선=공백
+ctx9.stroke();
+
+ctx9.beginPath();
+ctx9.moveTo(100, 100);
+ctx9.lineTo(300, 100);
+ctx9.setLineDash([20, 10]) //선-공백
+ctx9.stroke();
+
+ctx9.beginPath();
+ctx9.moveTo(100, 150);
+ctx9.lineTo(300, 150);
+ctx9.setLineDash([20, 10, 50, 10]) //패턴 선-공백-선-공백
+ctx9.stroke();
