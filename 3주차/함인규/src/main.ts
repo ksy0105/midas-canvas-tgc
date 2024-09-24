c0f62695d00a77cc7001fd9483a89313b80193be
HamInKyou <hfire1207@gmail.com>
2024-09-25 00:44:03 +0900

    [함인규] 3주차 :: 이동, 회전, 스케일, 변형하기

--- 3주차/함인규/src/main.ts
+++ 3주차/함인규/src/main.ts
@@ -406,3 +406,97 @@ new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
     ctx.fillStyle = "red";
     ctx.fillRect(50, 50, 100, 100);
 })
+
+//위치 이동시키기
+new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "green";
+    ctx.fillRect(50, 50, 100, 100);
+    ctx.translate(100,100); //다음에 선언하는 애의 위치를 변경
+    ctx.fillRect(50, 50, 100, 100); //처음이랑 동일한 위치지만 시작점 변경됨
+})
+
+//크기 변형하기
+new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "green";
+    ctx.fillRect(50, 50, 100, 100);
+    ctx.scale(0.5, 0.5); //다음에 선언하는 애의 크기를 변경
+    ctx.fillStyle = "red";
+    ctx.fillRect(50, 50, 100, 100);
+})
+
+//회전시키기
+new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(100, 100, 100, 100);
+    ctx.fillRect(100, 100, 100, 100);
+
+    ctx.rotate(5* Math.PI / 180); //Math.PI는 180도 -> 5 * 180/180 -> 5도
+    ctx.strokeRect(100, 100, 100, 100);
+    ctx.fillRect(100, 100, 100, 100);
+
+    ctx.rotate(5* Math.PI / 180);
+    ctx.strokeRect(100, 100, 100, 100);
+    ctx.fillRect(100, 100, 100, 100);
+})
+
+//사각형 형태 변형하기 - 확대와 이동
+new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 100, 100);
+
+    ctx.transform(1.5, 0, 0, 1.5, 100, 100); //수평확대비율, 수평기울임, 수직기울임, 수직확대비율, 수평이동, 수직이동
+    ctx.strokeRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 100, 100);
+})
+
+//사각형 형태 변형하기 - 기울임과 이동
+new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 100, 100);
+
+    ctx.transform(1, 0.2, 0.2, 1, 100, 100); //수평확대비율, 수평기울임, 수직기울임, 수직확대비율, 수평이동, 수직이동
+    ctx.strokeRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 100, 100);
+})
+
+//transform과 setTransform차이 - transform 우선일 경우
+new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 100, 100);
+
+    ctx.transform(1, 0.2, 0.2, 1, 100, 100); //수평확대비율, 수평기울임, 수직기울임, 수직확대비율, 수평이동, 수직이동
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 100, 100);
+
+    ctx.setTransform(1, 0, 0, 1, 100, 100); //transform 이시점부터 초기화!
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 100, 100);
+})
+
+//transform과 setTransform차이 - setTransform 우선일 경우
+new CanvasExample((_,ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 100, 100);
+
+    ctx.setTransform(1, 0.2, 0.2, 1, 100, 100); //transform 이 시점부터 초기화!
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 100, 100);
+
+    ctx.transform(1, 0, 0, 1, 100, 100); //setTransform 한 이후로 이어서~ (transform끼리 이어놓으면 변형 덧붙여짐 - 이어짐)
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 100, 100);
+})
