d6d4b4296870078c930aab49b662d53f229d647a
psy0909 <psy0909@midasin.com>
2024-09-26 11:16:38 +0900

    feat: 3.1 선그리기 ~ 3.4 이미지그리기

--- 3주차/박세영/src/main.ts
+++ 3주차/박세영/src/main.ts
@@ -1,24 +1,76 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const $canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const CANVAS_WIDTH = 400; 
+const CANVAS_HEIGHT = 300;
+$canvas.width = CANVAS_WIDTH;
+$canvas.height = CANVAS_HEIGHT;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const ctx = $canvas.getContext('2d')!;
+
+const 선_그리기 = () => {
+  ctx.beginPath();
+  ctx.moveTo(100, 50);
+  ctx.lineTo(300, 50);
+  ctx.stroke();
+}
+// 선_그리기();
+
+const 사각형_그리기 = () => {
+  ctx.beginPath();
+  ctx.moveTo(100, 50);
+  ctx.lineTo(300, 50);
+  ctx.lineTo(300, 200);
+  ctx.lineTo(100, 200);
+  ctx.lineTo(100, 50);
+  ctx.stroke();
+  ctx.fill();
+}
+// 사각형_그리기();
+
+const 내부가_채워진_사각형_그리기 = ()=>{
+  ctx.fillStyle = 'magenta';
+  ctx.fillRect(20, 20, 100, 100);
+  ctx.strokeRect(20, 20, 100, 100);
+  ctx.clearRect(50,50,50,50);
+
+  ctx.fillStyle = 'green';
+  ctx.fillRect(150, 150, 50, 50);
+  ctx.strokeRect(150, 150, 50, 50);
+}
+// 내부가_채워진_사각형_그리기();
+
+const 기본_원_그리기 = () => {
+  ctx.arc(150, 150, 100, 0, 2*Math.PI);
+  ctx.stroke();
+}
+// 기본_원_그리기();
+
+const 선과_호를_연결하여_라운드_코너_그리기 = () => {
+  ctx.beginPath();
+  ctx.moveTo(50, 50);
+  ctx.lineTo(300, 50);
+  ctx.arcTo(350, 50, 350, 100, 50);
+  ctx.stroke();
+}
+// 선과_호를_연결하여_라운드_코너_그리기();
+
+const radio_gradient로_사각형_내부_채우기 = () => {
+  const grad = ctx.createRadialGradient(0, 0, 0, 100, 100, 300);
+  grad.addColorStop(0, "red");
+  grad.addColorStop(0.5, "purple");
+  grad.addColorStop(1, "blue");
+  ctx.fillStyle = grad;
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+// radio_gradient로_사각형_내부_채우기();
+
+const 패턴으로_사각형_내부_채우기 = () => {
+  const flowerImg = new Image();
+  flowerImg.src = '../assets/images/flower_pattern.png';
+  flowerImg.onload = () => {
+    const pattern = ctx.createPattern(flowerImg, "repeat")!;
+    ctx.fillStyle = pattern;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+// 패턴으로_사각형_내부_채우기();
