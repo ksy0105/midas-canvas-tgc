ac2307f0b90f61e858db1661a152dfd533b0f81d
psy0909 <psy0909@midasin.com>
2024-09-26 12:51:18 +0900

    feat: 3.5 이미지 그리기 ~ 3.9 색상 보정하기

--- 3주차/박세영/src/main.ts
+++ 3주차/박세영/src/main.ts
@@ -5,6 +5,8 @@ const CANVAS_HEIGHT = 300;
 $canvas.width = CANVAS_WIDTH;
 $canvas.height = CANVAS_HEIGHT;
 
+const getAssetPath = (fileName: string) => `../assets/images/${fileName}`
+
 const ctx = $canvas.getContext('2d')!;
 
 const 선_그리기 = () => {
@@ -66,7 +68,7 @@ const radio_gradient로_사각형_내부_채우기 = () => {
 
 const 패턴으로_사각형_내부_채우기 = () => {
   const flowerImg = new Image();
-  flowerImg.src = '../assets/images/flower_pattern.png';
+  flowerImg.src = getAssetPath('flower_pattern.png');
   flowerImg.onload = () => {
     const pattern = ctx.createPattern(flowerImg, "repeat")!;
     ctx.fillStyle = pattern;
@@ -74,3 +76,89 @@ const 패턴으로_사각형_내부_채우기 = () => {
   }
 }
 // 패턴으로_사각형_내부_채우기();
+
+const 이미지를_잘라_일부만_그리기 = () => {
+  const catImg = new Image();
+  catImg.src = getAssetPath('street_cat.jpg');
+  catImg.onload = () => {
+    ctx.drawImage(catImg, 100, 100, 200, 200, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+// 이미지를_잘라_일부만_그리기();
+
+const 글씨의_외곽선_그리기 = () => {
+  ctx.fillStyle = 'black';
+
+  ctx.font = "italic bold 40px Arial, sans-serif"; //글씨 폰트 스타일 변경
+  ctx.fillText('Hello World', 10, 100);
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'yellow'
+  ctx.strokeText('Hello World', 10, 100);
+}
+// 글씨의_외곽선_그리기();
+
+const 그림자_그리기 = () => {
+  ctx.shadowColor = "rgba(0, 0, 0, 0.3)";
+  ctx.shadowOffsetX = 5;
+  ctx.shadowOffsetY = 5;
+  ctx.shadowBlur = 3;
+  ctx.fillStyle = "red";
+  ctx.fillRect(50, 50, 100, 100);
+};
+// 그림자_그리기();
+
+const 위치_이동하기 = () => {
+  ctx.fillStyle = 'green';
+  ctx.fillRect(50, 50, 100, 100);
+  ctx.translate(40, 40);
+  ctx.fillRect(50, 50, 100, 100);
+};
+// 위치_이동하기();
+
+const 크기_변형하기 = () => {
+  ctx.fillStyle = 'green';
+  ctx.fillRect(50, 50, 100, 100);
+  ctx.scale(0.5, 0.5);
+  ctx.fillRect(50, 50, 100, 100);
+};
+// 크기_변형하기();
+
+const 회전시키기 = () => {
+  ctx.fillStyle = 'green';
+  ctx.strokeRect(100, 100, 100, 100);
+  ctx.rotate(0.1);
+  ctx.strokeRect(100, 100, 100, 100);
+  ctx.rotate(0.1);
+  ctx.strokeRect(100, 100, 100, 100);
+};
+// 회전시키기();
+
+const 사각형_형태_변형하기 = () => {
+  ctx.fillStyle = 'yellow';
+  ctx.fillRect(20, 20, 100, 100);
+  // ctx.transform(1, 0.2, 0, 1, 120, 120);
+  ctx.transform(1, 0, 0.2, 1, 120, 120);
+  ctx.fillRect(20, 20, 100, 100);
+};
+// 사각형_형태_변형하기();
+
+const 이미지_색상_반전시키기 = () => {
+  const catImg = new Image();
+  catImg.src = getAssetPath('street_cat.jpg');
+  catImg.onload = () => {
+    ctx.drawImage(catImg, 50, 40, 400, 300, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    const imgData = ctx.getImageData(200, 100, 100, 100);
+    const imgRaw = imgData.data;
+
+    for(let i=0; i < imgRaw.length; i+=4) {
+      imgRaw[i] = 255 - imgRaw[i];
+      imgRaw[i+1] = 255 - imgRaw[i+1];
+      imgRaw[i+2] = 255 - imgRaw[i+2];
+    }
+    
+    ctx.putImageData(imgData, 200, 100)
+  }
+}
+이미지_색상_반전시키기()
