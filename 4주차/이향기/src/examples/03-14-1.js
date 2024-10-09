ef00581391f967d1ba4f38dd682440503f878182
2scent <ad7549@gmail.com>
2024-10-10 01:04:42 +0900

    [이향기] 4주차 :: 이미지를 키보드로 움직이기

---
+++ 4주차/이향기/src/examples/03-14-1.js
@@ -0,0 +1,36 @@
+import getCanvas from "../get-canvas.js";
+
+const { canvas, ctx } = getCanvas('03-14-1');
+
+const bgImage = new Image();
+bgImage.src = 'dog.jpg';
+const playerImage = new Image();
+playerImage.src = 'fire.webp';
+let x = 0;
+
+function Background() {
+  this.x = 0, this.y = 0, this.w = bgImage.width, this.h = bgImage.height;
+  this.render = function() {
+    ctx.drawImage(bgImage, x--, -250);
+    if (x <= -400) x = 0;
+  };
+}
+
+function Player() {
+  this.x = 0, this.y = 0, this.w = playerImage.width, this.h = playerImage.height;
+  this.render = function() {
+    ctx.drawImage(playerImage, this.x, this.y);
+  };
+}
+
+const background = new Background();
+const player = new Player();
+player.x = 30;
+player.y = 0;
+
+function animate() {
+  background.render();
+  player.render();
+}
+
+const animateInterval = setInterval(animate, 15);
